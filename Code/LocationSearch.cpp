92f186e8e9371ae7c34dad227788a7b9cf4feefa
Interagus <adityan.sk@hotmail.com>
2020-10-18 15:33:13 +0530

    Added Visualization And Timed the Algo

--- Code/LocationSearch.cpp
+++ Code/LocationSearch.cpp
@@ -1,16 +1,11 @@
 #include "PersonTree.hxx"
+#include "../CUDA/Timer.hxx"
 using namespace std;
 map<pair<int,string>,vector<int>> idVec;
 void addtoVec(string code,string date, vector<int>contactArr){
     for(vector<int>::iterator it = locs.at(code).begin(); it!=locs.at(code).end();it++){
         if(p[*it].wasPresent(date)){
             vector<int>temp = contactArr;
-            // try{
-            //    temp = idVec.at({*it,date}); 
-            // }
-            // catch(const std::exception& e){
-            //     std::cerr << e.what() << '\n';
-            // }
             temp.push_back(*it);
             idVec.insert(pair<pair<int,string>,vector<int>>({*it,date},temp));
         }
@@ -20,24 +15,26 @@ int main(){
     CreateGraph();
     vector<int>contactArr;
     string loc_code, area_code, date;
-    cout<<"Enter Location Code: "<<'\n';
+    cout<<"Enter Location Code: ";
     cin>>loc_code;
-    cout<<"Enter Area Code: "<<'\n';
+    cout<<"Enter Area Code: ";
     cin>>area_code;
-    cout<<"Enter Date (yyyy-mm-dd)";
+    cout<<"Enter Date (yyyy-mm-dd): ";
     cin>>date;
+    Timer clock;
     addtoVec(loc_code+area_code, date, contactArr);
     bool flag = false;
     for(map<pair<int,string>,vector<int>>::iterator it = idVec.begin(); it!=idVec.end();it++){
         if(p[it->first.first].getStatus()){
+            double time = clock.Stop();
             cout<<"Possibility of Infection Found...\n";
             cout<<"Infection Chain: ";
-            // cout<<it->first.first<<' ';
             flag = true;
             for (vector<int>::iterator i = it->second.begin(); i != it->second.end(); i++){
                 cout<<*i<<"<--";
             }
             cout<<"Infection\n";
+            cout<<"Time Taken: "<<time<<'\n';
             
             break;
         }else{
