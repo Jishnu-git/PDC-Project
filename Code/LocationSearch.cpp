b8656bd4b335a2b3494cf76bbb4b0ba067881f2f
Interagus <adityan.sk@hotmail.com>
2020-10-18 15:17:25 +0530

    Algo Correction and Added Visualization

--- Code/LocationSearch.cpp
+++ Code/LocationSearch.cpp
@@ -1,15 +1,24 @@
 #include "PersonTree.hxx"
 using namespace std;
-map<pair<int,string>,bool> idVec;
-void addtoVec(string code,string date){
+map<pair<int,string>,vector<int>> idVec;
+void addtoVec(string code,string date, vector<int>contactArr){
     for(vector<int>::iterator it = locs.at(code).begin(); it!=locs.at(code).end();it++){
-           if(p[*it].wasPresent(date)){
-                idVec.insert(pair<pair<int,string>,bool>({*it,date},p[*it].getStatus()));
-           }
-       }
+        if(p[*it].wasPresent(date)){
+            vector<int>temp = contactArr;
+            // try{
+            //    temp = idVec.at({*it,date}); 
+            // }
+            // catch(const std::exception& e){
+            //     std::cerr << e.what() << '\n';
+            // }
+            temp.push_back(*it);
+            idVec.insert(pair<pair<int,string>,vector<int>>({*it,date},temp));
+        }
+    }
 }
 int main(){
     CreateGraph();
+    vector<int>contactArr;
     string loc_code, area_code, date;
     cout<<"Enter Location Code: "<<'\n';
     cin>>loc_code;
@@ -17,37 +26,33 @@ int main(){
     cin>>area_code;
     cout<<"Enter Date (yyyy-mm-dd)";
     cin>>date;
-
-
-    try{
-       //idVec = locs.at(loc_code+area_code);
-       
-    }
-    catch(const std::exception& e){
-        cout<<"No data regarding given location and area";
-        return 0;
-    }
+    addtoVec(loc_code+area_code, date, contactArr);
     bool flag = false;
-    for(map<pair<int,string>,bool>::iterator it = idVec.begin(); it!=idVec.end();it++){
-        if(it->second){
+    for(map<pair<int,string>,vector<int>>::iterator it = idVec.begin(); it!=idVec.end();it++){
+        if(p[it->first.first].getStatus()){
+            cout<<"Possibility of Infection Found...\n";
+            cout<<"Infection Chain: ";
+            // cout<<it->first.first<<' ';
             flag = true;
+            for (vector<int>::iterator i = it->second.begin(); i != it->second.end(); i++){
+                cout<<*i<<"<--";
+            }
+            cout<<"Infection\n";
+            
             break;
         }else{
             vector<pair<string,vector<string>>> templocs;
             templocs = p[it->first.first].getDateRange(it->first.second,5);
-            for (vector<pair<string,vector<string>>>::iterator it = templocs.begin(); it != templocs.end(); it++){
-                for(vector<string>::iterator i=(*it).second.begin();i!=(*it).second.end();i++){
-                    addtoVec(*i,(*it).first);
+            for (vector<pair<string,vector<string>>>::iterator iter = templocs.begin(); iter != templocs.end(); iter++){
+                for(vector<string>::iterator i=(*iter).second.begin();i!=(*iter).second.end();i++){
+                    addtoVec(*i,(*iter).first, it->second);
                 }
             }
             
         }
     }
-    if(flag){
-        cout<<"Infected";
-    }else{
+    if(!flag){
         cout<<"Not Infected";
-    }
-    
+    }    
     return 0;
 }
