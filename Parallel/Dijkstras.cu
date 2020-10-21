c86430edf75443c2b2e73354bd77945100fd82df
Jishnu <jishnu30alamelu26@gmail.com>
2020-10-22 00:39:00 +0530

    Changed the search to loop instead of terminating

--- Parallel/Dijkstras.cu
+++ Parallel/Dijkstras.cu
@@ -27,72 +27,73 @@ int main(void) {
     cudaMemcpy(d_Status, h_Status, sizeof(bool) * Status.size(), cudaMemcpyHostToDevice);
     delete h_Status;
 
-    vector<long long> neighbors;
-    bool *visited;
-    long long *solution;
-    cudaMallocManaged((void**)&solution, sizeof(long long));
-    cudaMallocManaged((void**)&visited, sizeof(bool) * People.size());
-    *solution = -1;
-    
-    cout << "Enter ID of person: ";
-    long long id; cin >> id;
-
-    if (id < 1 || id >= People.size()) {
-        cerr << "\nID out of range\n";
-        return -1;
-    }
-    neighbors.push_back(id);
-
-    cout << "\nProcessing...";
-    Timer timer, op;
-    while (!neighbors.empty() && *solution == -1) {
-        if (op.GetTime() >= 0.1) {
-            cout << "\rEntries Processed: " << count_if(visited, visited + People.size(), [](bool status) {
-                return status;
-            });
-            cout.flush();
-            op.Reset();
-        }
-
-        long long *d_neighbors;
-        cudaMalloc((void**)&d_neighbors, sizeof(long long) * neighbors.size());
-        cudaMemcpy(d_neighbors, &neighbors[0], sizeof(long long) * neighbors.size(), cudaMemcpyHostToDevice);
-        isInfected<<<neighbors.size() / MAX + 1, MAX>>>(d_neighbors, solution, visited, d_Status, neighbors.size());
-        cudaDeviceSynchronize();
-        cudaFree(d_neighbors);
+    while (true) {
+        vector<long long> neighbors;
+        bool *visited;
+        long long *solution;
+        cudaMallocManaged((void**)&solution, sizeof(long long));
+        cudaMallocManaged((void**)&visited, sizeof(bool) * People.size());
+        *solution = -1;
         
-        if (*solution != -1) {
+        cout << "Enter ID of person: ";
+        long long id; cin >> id;
+
+        if (id < 1 || id >= People.size()) {
+            cerr << "\nID out of range\n";
+            return -1;
+        }
+        neighbors.push_back(id);
+
+        cout << "\nProcessing...";
+        Timer timer, op;
+        while (!neighbors.empty() && *solution == -1) {
+            if (op.GetTime() >= 0.1) {
+                cout << "\rEntries Processed: " << count_if(visited, visited + People.size(), [](bool status) {
+                    return status;
+                });
+                cout.flush();
+                op.Reset();
+            }
+
+            long long *d_neighbors;
+            cudaMalloc((void**)&d_neighbors, sizeof(long long) * neighbors.size());
+            cudaMemcpy(d_neighbors, &neighbors[0], sizeof(long long) * neighbors.size(), cudaMemcpyHostToDevice);
+            isInfected<<<neighbors.size() / MAX + 1, MAX>>>(d_neighbors, solution, visited, d_Status, neighbors.size());
+            cudaDeviceSynchronize();
+            cudaFree(d_neighbors);
+            
+            if (*solution != -1) {
+                cout << "\rEntries Processed: " << count_if(visited, visited + People.size(), [](bool status) {
+                    return status;
+                }) 
+                    << "... Done" << endl;
+                cout << "Closest Infected Person: \n" << People[*solution].info() << endl;
+                break;
+            }
+            
+            vector<long long> newNeighbors;
+            for (auto neighbor: neighbors) {
+                copy_if(Graph[neighbor - 1].begin(), Graph[neighbor - 1].end(), back_inserter(newNeighbors), [visited](long long id) {
+                    return !visited[id];
+                });
+            }
+            neighbors.clear();
+            neighbors = newNeighbors;
+        }
+        double time = timer.Stop();
+
+        if (*solution == -1) {
             cout << "\rEntries Processed: " << count_if(visited, visited + People.size(), [](bool status) {
                 return status;
             }) 
-                 << "... Done" << endl;
-            cout << "Closest Infected Person: \n" << People[*solution].info() << endl;
-            break;
+                << "... Done" << endl;
+            cout << "\nPerson has not come in contact with an infected person\n";
         }
-        
-        vector<long long> newNeighbors;
-        for (auto neighbor: neighbors) {
-            copy_if(Graph[neighbor - 1].begin(), Graph[neighbor - 1].end(), back_inserter(newNeighbors), [visited](long long id) {
-                return !visited[id];
-            });
-        }
-        neighbors.clear();
-        neighbors = newNeighbors;
+
+        cout << "\nTime Taken: " << time << endl;
+
+        cudaFree(solution);
+        cudaFree(visited);    
     }
-    double time = timer.Stop();
-
-    if (*solution == -1) {
-        cout << "\rEntries Processed: " << count_if(visited, visited + People.size(), [](bool status) {
-            return status;
-        }) 
-             << "... Done" << endl;
-        cout << "\nPerson has not come in contact with an infected person\n";
-    }
-
-    cout << "\nTime Taken: " << time << endl;
-
-    cudaFree(solution);
-    cudaFree(visited);
-    
     return 0;
 }
